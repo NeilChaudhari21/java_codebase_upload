afb96991db6d3befd9d30595fede931b0479a437
Neil Chaudhari <126905080+NeilChaudhari21@users.noreply.github.com>
2026-06-30 15:37:01 -0700

    Delete enrollment-report-service directory

--- enrollment-report-service/src/main/java/com/hcas/enrollmentreport/repository/EnrollmentErrorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hcas.enrollmentreport.repository;
-
-import com.hcas.enrollmentreport.model.EnrollmentError;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentErrorRepository extends JpaRepository<EnrollmentError, Long> {
-}

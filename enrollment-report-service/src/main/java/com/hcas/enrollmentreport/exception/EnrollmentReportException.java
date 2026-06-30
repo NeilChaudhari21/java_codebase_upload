d9dd29cffb7f10dee40f677bdc752610c3ffe3f8
Neil Chaudhari <126905080+NeilChaudhari21@users.noreply.github.com>
2026-06-30 14:19:49 -0700

    Delete enrollment-report-service directory

--- enrollment-report-service/src/main/java/com/hcas/enrollmentreport/exception/EnrollmentReportException.java
+++
@@ -1,7 +0,0 @@
-package com.hcas.enrollmentreport.exception;
-
-public class EnrollmentReportException extends RuntimeException {
-    public EnrollmentReportException(String message) {
-        super(message);
-    }
-}
